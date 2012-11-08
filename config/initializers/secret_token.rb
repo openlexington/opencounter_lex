f1e28ddb61b7efc3fdae28de8eec44af1f30eef7
blakeshall <blake@blakeshall.com>
2012-11-07 20:52:05 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lexopencounter::Application.config.secret_token = '060eb8858c8529a990e8383bb6cb3a5be84716598c360b235de6234441aaafd8b6e7c97b93da474781da1897f5c299461971a1f16730623600e80892d3fe18be'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Lexopencounter::Application.config.session_store :cookie_store, key: '_lexopencounter_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Lexopencounter::Application.config.session_store :active_record_store
